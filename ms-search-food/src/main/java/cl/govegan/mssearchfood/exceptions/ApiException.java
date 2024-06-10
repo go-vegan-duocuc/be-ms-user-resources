66def924830165bb1024467325a070e3b2086687
Gab-Serrano <gab.serrano@duocuc.cl>
2024-06-10 10:19:45 -0400

    port ms

--- ms-search-food/src/main/java/cl/govegan/mssearchfood/exceptions/ApiException.java
+++
@@ -1,7 +0,0 @@
-package cl.govegan.mssearchfood.exceptions;
-
-public class ApiException extends RuntimeException {
-   public ApiException(String message) {
-       super(message);
-   }
-}
--- ms-search-food/src/main/java/cl/govegan/mssearchfood/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cl.govegan.mssearchfood.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
