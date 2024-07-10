590233194d66b25faad585e821e4a19c13650852
Gab-Serrano <gab.serrano@duocuc.cl>
2024-07-10 11:15:52 -0400

    #

--- ms-user-resources/src/main/java/cl/govegan/msuserresources/MsUserResourcesApplication.java
+++ ms-user-resources/src/main/java/cl/govegan/msuserresources/MsUserResourcesApplication.java
@@ -1,9 +1,11 @@
 package cl.govegan.msuserresources;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MsUserResourcesApplication {
 
 	public static void main(String[] args) {
